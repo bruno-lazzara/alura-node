08fcb58f73a86c0d323f55810985db9f9ac64d23
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2023-09-04 08:13:32 -0300

    Aplica paginação na listagem de livros

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import ErroBase from '../errors/ErroBase.js';
 import RequisicaoIncorreta from '../errors/RequisicaoIncorreta.js';
 import ErroValidacao from '../errors/ErroValidacao.js';
-import NaoEncontrado from '../errors/NaoEncontrado.js';
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(err, req, res, next) {
@@ -10,7 +9,7 @@ function manipuladorDeErros(err, req, res, next) {
         new RequisicaoIncorreta().enviarResposta(res);
     } else if (err instanceof mongoose.Error.ValidationError) {
         new ErroValidacao(err).enviarResposta(res);
-    } else if (err instanceof NaoEncontrado) {
+    } else if (err instanceof ErroBase) {
         err.enviarResposta(res);
     } else {
         new ErroBase().enviarResposta(res);
